be5992b0b4fffe310762acd64694170e77fe40b2
SobaRiz <119413044+SobaRiz@users.noreply.github.com>
2023-02-17 09:41:01 +0000

    Ajout du projet 'EasySave'

---
+++ src/CraftLR.EasySave/Views/MainWindowView.axaml.cs
@@ -0,0 +1,14 @@
+using System;
+
+using Avalonia.Controls;
+
+// using CraftLR.EasySave.ViewModels;
+namespace CraftLR.EasySave.Views;
+
+public partial class MainWindowView : Window
+{
+    public MainWindowView()
+    {
+        InitializeComponent();
+    }
+}
